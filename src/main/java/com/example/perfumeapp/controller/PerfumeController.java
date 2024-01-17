ed0081b723f7442205fba49505a6fe8e3340d364
AmatheGirl <hoseaamayindi19@gmail.com>
2024-01-17 20:50:12 +0100

    ,,

--- src/main/java/com/example/perfumeapp/controller/PerfumeController.java
+++ src/main/java/com/example/perfumeapp/controller/PerfumeController.java
@@ -3,13 +3,14 @@ package com.example.perfumeapp.controller;
 import com.example.perfumeapp.Domain.Perfume;
 import com.example.perfumeapp.Service.PerfumeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+@Controller
 public class PerfumeController {
     @Autowired
     PerfumeService perfumeService;
--- src/main/java/com/example/perfumeapp/controller/api/PerfumeAPI.java
+++ src/main/java/com/example/perfumeapp/controller/api/PerfumeAPI.java
@@ -6,10 +6,11 @@ import com.example.perfumeapp.Service.PerfumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Controller
 public class PerfumeAPI {
 
     @Autowired
