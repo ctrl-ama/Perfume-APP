8706eb8e9a4698ccd8553d687b1267fd1fd14f68
AmatheGirl <hoseaamayindi19@gmail.com>
2024-01-18 01:06:58 +0100

    ,,

---
+++ src/main/java/com/example/perfumeapp/PerfumeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfumeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfumeAppApplication.class, args);
+    }
+
+}
