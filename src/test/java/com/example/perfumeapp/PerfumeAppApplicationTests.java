e04eef05d2e0fea74a3d5c939c48896e6640850d
bOLA-532 <faithadebolaibrahim@gmail.com>
2024-01-18 11:25:45 -0800

    ;)

--- src/test/java/com/example/perfumeapp/PerfumeAppApplicationTests.java
+++ src/test/java/com/example/perfumeapp/PerfumeAppApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PerfumeAppApplicationTests {
+public class PerfumeAppApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
